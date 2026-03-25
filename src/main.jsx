79a3c181ff97890b0ea8121940b510c6b11cf795
Mickaelly <mickaellysrs@gmail.com>
2026-03-25 11:34:54 -0300

    fix: corrigindo case-sensitivity no main e imports

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { CartProvider } from './contexts/cartContext';
-import App from './App'; // Verifique se o arquivo na pasta src se chama App.jsx (com A maiúsculo)
+import App from './app'; // Verifique se o arquivo na pasta src se chama App.jsx (com A maiúsculo)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
